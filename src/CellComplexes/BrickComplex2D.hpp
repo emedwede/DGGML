7af1814c33b1d2631412d340a5a400e10784ea11
Eric Medwedeff <emedwede@uci.edu>
2021-12-01 17:33:35 -0800

    Added the empty class for the brick complex(aka semi-regular grid cell complex)

---
+++ src/CellComplexes/BrickComplex2D.hpp
@@ -0,0 +1,15 @@
+#ifndef CAJETE_BRICK_COMPLEX_2D_HPP
+#define CAJETE_BRICK_COMPLEX_2D_HPP
+
+#include "CellComplexTypes.hpp"
+
+namespace Cajete
+{
+    template <typename graph_type = CplexGraph2D_t>
+    class BrickComplex //The brick cell complex is a semi-regular grid
+    {
+        //TODO: implement the cell complex for a semi-regular grid
+    };
+} // end namespace Cajete
+
+#endif
