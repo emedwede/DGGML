111a4a15f18ae3486a28c1371b7529154966c460
Eric Medwedeff <ejm3a2@gmail.com>
2023-11-14 15:57:38 -0800

    Moved key generator to its own file

--- src/ComponentMap.hpp
+++ src/ComponentMap.hpp
@@ -10,6 +10,7 @@
 #include "YAGL_Algorithms.hpp"
 #include "YAGL_Graph.hpp"
 #include "AnalyzedGrammar.hpp"
+#include "KeyGenerator.hpp"
 
 namespace DGGML
 {
@@ -30,22 +31,6 @@ namespace DGGML
             os << "Intermediate";
         return os;
     }
-
-    //We need matching rewrite functions for every enum class member
-
-    template<typename KeyType>
-    struct KeyGenerator 
-    {
-        using key_type = KeyType;
-        
-        key_type current_key;
-
-        KeyGenerator(KeyType key) : current_key(key) {}
-
-        KeyGenerator() : current_key(0) {}
-
-        key_type get_key() { return current_key++; }
-    };
 
     template <typename KeyType>
     struct RuleInstType {
---
+++ src/Utlities/KeyGenerator.hpp
@@ -0,0 +1,18 @@
+#ifndef DGGML_KEYGENERATOR_HPP
+#define DGGML_KEYGENERATOR_HPP
+
+template<typename KeyType>
+struct KeyGenerator
+{
+    using key_type = KeyType;
+
+    key_type current_key;
+
+    KeyGenerator(KeyType key) : current_key(key) {}
+
+    KeyGenerator() : current_key(0) {}
+
+    key_type get_key() { return current_key++; }
+};
+
+#endif //DGGML_KEYGENERATOR_HPP
